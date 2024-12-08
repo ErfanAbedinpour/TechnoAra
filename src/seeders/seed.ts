2175869563be10d4afa0126461bf4480ae39876b
erfan <erfan.wtf44@gmail.com>
2024-12-08 12:40:24 -0500

    chore: Hashing passwords

--- src/seeders/seed.ts
+++ src/seeders/seed.ts
@@ -1,19 +1,22 @@
-import { EntityManager } from '@mikro-orm/core';
-import { Seeder } from '@mikro-orm/seeder';
+import { EntityManager } from '@mikro-orm/core'; import { Seeder } from '@mikro-orm/seeder';
 import { Role, UserRole } from '../models/role.model';
 import { UserFactory } from './factoryies.entity';
 import { User } from '../models/user.model';
+import { ArgonService } from '../modules/auth/hashingServices/argon.service';
 
 
 
 
 export class DatabaseSeeder extends Seeder {
 
+    hashService = new ArgonService();
     async run(em: EntityManager): Promise<void> {
         em.create(Role, { id: 1, name: UserRole.ADMIN });
         em.create(Role, { id: 2, name: UserRole.USER });
         await em.flush();
-        em.create(User, { id: 1, username: "admin", email: "admin@gmail.com", password: "Test11223344_" })
-        new UserFactory(em).make(10);
+        const password = await this.hashService.hash("Test11223344_")
+        em.create(User, { id: 1, username: "admin", email: "admin@gmail.com", password, role: 1 })
+        new UserFactory(em).make(10, { password, role: 2 })
     }
+
 }
