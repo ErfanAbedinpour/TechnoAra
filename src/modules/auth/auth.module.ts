7136f37ddc9dceb43022c8647d1f3139de6b9421
erfan <erfan.wtf44@gmail.com>
2024-12-09 09:17:17 -0500

    chore: store token into redis

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -5,19 +5,20 @@ import { MikroOrmModule } from "@mikro-orm/nestjs";
 import { User } from "src/models/user.model";
 import { HashService } from "./hashingServices/hash.service";
 import { ArgonService } from "./hashingServices/argon.service";
-import { AccessTokenService } from "./jwt/accessToken.service";
+import { AccessTokenService } from "./tokens/accessToken.service";
 import { JwtModule } from "@nestjs/jwt";
 import { ConfigModule } from "@nestjs/config";
 import accessTokenConfig from "./config/accessToken.config";
 import refreshTokenConfig from "./config/refreshToken.config";
-import { RefreshTokenService } from "./jwt/refreshToken.service";
+import { RefreshTokenService } from "./tokens/refreshToken.service";
+import { UserTokenService } from "./tokens/user.token.service";
 
 
 @Module({
     imports: [
         MikroOrmModule.forFeature({ entities: [User] }),
         JwtModule.register({}),
-        ConfigModule.forRoot({ load: [accessTokenConfig, refreshTokenConfig] })
+        ConfigModule.forRoot({ load: [accessTokenConfig, refreshTokenConfig] }),
     ],
     providers: [AuthService,
         {
@@ -25,7 +26,8 @@ import { RefreshTokenService } from "./jwt/refreshToken.service";
             useClass: ArgonService
         },
         AccessTokenService,
-        RefreshTokenService
+        RefreshTokenService,
+        UserTokenService
     ],
     controllers: [AuthController],
 })
--- src/modules/redis/redis.module.ts
+++ src/modules/redis/redis.module.ts
@@ -1,6 +1,7 @@
-import { Module } from "@nestjs/common";
+import { Global, Module } from "@nestjs/common";
 import { RedisRepository } from "./redis.repository";
 
+@Global()
 @Module({
     exports: [RedisRepository],
     providers: [RedisRepository],
