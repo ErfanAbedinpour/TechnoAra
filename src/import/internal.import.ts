1a15a0ed4e25141efede61b9de5e5ee1e5392db6
erfan <erfan.wtf44@gmail.com>
2024-12-09 07:29:58 -0500

    chore: Adding redis module to app

--- src/import/internal.import.ts
+++ src/import/internal.import.ts
@@ -1,3 +1,4 @@
 import { AuthModule } from "src/modules/auth/auth.module";
+import { ReidsModule } from "../modules/redis/redis.module";
 
-export const internalImports = [AuthModule];
+export const internalImports = [AuthModule, ReidsModule];
