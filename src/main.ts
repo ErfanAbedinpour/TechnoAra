27238e84836236cf1b360c0e46fc5933a300e420
erfan <erfan.wtf44@gmail.com>
2024-12-09 14:16:07 -0500

    chore: Adding swagger basic

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MikroORM } from '@mikro-orm/core';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,9 +11,15 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
   await app.get(MikroORM).getSchemaGenerator().ensureDatabase();
 
-  await app.get(MikroORM).getSchemaGenerator().updateSchema();
-
   app.setGlobalPrefix("api")
+  const config = new DocumentBuilder()
+    .setTitle('TechnoAra Api ')
+    .setDescription('The technoAra API description')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, documentFactory);
+
   await app.listen(port, () => {
     console.log(`http://localhost:${port}`)
   });
