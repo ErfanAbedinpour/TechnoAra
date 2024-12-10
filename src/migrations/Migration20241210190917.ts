2a1c0c31e0a23f803e654ee7faa6698cf0f220b5
erfan <erfan.wtf44@gmail.com>
2024-12-10 17:03:17 -0500

    chore: Updating user model Added Wallet to user model One to one Relations

---
+++ src/migrations/Migration20241210190917.ts
@@ -0,0 +1,18 @@
+import { Migration } from '@mikro-orm/migrations';
+
+export class Migration20241210190917 extends Migration {
+
+  override async up(): Promise<void> {
+    this.addSql(`alter table "users" drop constraint "users_wallet_id_foreign";`);
+
+    this.addSql(`alter table "users" drop constraint "users_wallet_id_unique";`);
+    this.addSql(`alter table "users" drop column "wallet_id";`);
+  }
+
+  override async down(): Promise<void> {
+    this.addSql(`alter table "users" add column "wallet_id" int not null;`);
+    this.addSql(`alter table "users" add constraint "users_wallet_id_foreign" foreign key ("wallet_id") references "wallets" ("id") on update cascade;`);
+    this.addSql(`alter table "users" add constraint "users_wallet_id_unique" unique ("wallet_id");`);
+  }
+
+}
