3dfa7beb46911cc115bf1813a90fb43fa40d0137
erfan <erfan.wtf44@gmail.com>
2024-12-10 17:04:20 -0500

    chore: Import user Module

--- src/import/internal.import.ts
+++ src/import/internal.import.ts
@@ -1,5 +1,5 @@
 import { AuthModule } from "src/modules/auth/auth.module";
 import { ReidsModule } from "../modules/redis/redis.module";
-import { UserModule } from "../user/user.module";
+import { UserModule } from "../modules/user/user.module";
 
 export const internalImports = [AuthModule, ReidsModule, UserModule];
