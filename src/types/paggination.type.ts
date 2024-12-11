b7e002a2ede59402e8f9c8051a9b4e285283ff6a
erfan <erfan.wtf44@gmail.com>
2024-12-11 13:04:22 -0500

    chore: Adding Pagination Class

---
+++ src/types/paggination.type.ts
@@ -0,0 +1,13 @@
+import { Type } from "class-transformer";
+import { IsNotEmpty, Min } from "class-validator";
+
+export class Paggination {
+    @IsNotEmpty({ message: "please enter limit in query param" })
+    @Type(() => Number)
+    @Min(1)
+    limit: number;
+    @IsNotEmpty({ message: "please enter page in query param" })
+    @Type(() => Number)
+    @Min(1)
+    page: number;
+}
