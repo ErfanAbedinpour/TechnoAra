6a91bfebc2738bea79c2a7bd16cab520d312f074
erfan <erfan.wtf44@gmail.com>
2024-12-16 10:10:26 -0500

    chore: Move all Error Messages to spread Class

---
+++ src/errorResponse/err.response.ts
@@ -0,0 +1,9 @@
+export class ErrorMessages {
+    static readonly INVALID_NAME = ""
+    static readonly INVALID_SLUG = "slug is unavalible."
+    static readonly CATEGORY_NOT_FOUNT = "category does not found."
+    static readonly INVALID_EMAIL = 'email is registered by another user.';
+    static readonly INVALID_CRENDENTIAL = 'email or password incorrect';
+    static readonly INVALID_REFRESH_TOKEN = 'token is invaid';
+    static readonly USER_NOT_FOUND = 'account not found.';
+}
