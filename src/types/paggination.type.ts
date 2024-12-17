5ce4fe14762febaf82b01129ec3f854fb61d75b8
erfan <erfan.wtf44@gmail.com>
2024-12-17 15:24:12 -0500

    fix: Set defualt value for paggination values

--- src/types/paggination.type.ts
+++ src/types/paggination.type.ts
@@ -1,16 +1,14 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { Type } from "class-transformer";
-import { IsNotEmpty, Min } from "class-validator";
+import { Min } from "class-validator";
 
 export class Pagination {
     @ApiProperty()
-    @IsNotEmpty({ message: "please enter limit in query param" })
     @Type(() => Number)
     @Min(1)
-    limit: number;
+    limit: number = 10;
     @ApiProperty()
-    @IsNotEmpty({ message: "please enter page in query param" })
     @Type(() => Number)
     @Min(1)
-    page: number;
+    page: number = 1;
 }
