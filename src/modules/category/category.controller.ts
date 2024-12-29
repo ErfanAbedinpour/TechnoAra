be7dba770324181a051a9fb262e45a702208d10b
erfan <erfan.wtf44@gmail.com>
2024-12-29 15:51:10 -0500

    docs: Adding swagget docs to category

--- src/modules/category/category.controller.ts
+++ src/modules/category/category.controller.ts
@@ -6,7 +6,7 @@ import { Role } from '../auth/decorator/role.decorator';
 import { UserRole } from '../../models/role.model';
 import { GetUser } from '../auth/decorator/get-user.decorator';
 import { SlugifyInterceptor } from '../../interceptor/slugify.interceptor';
-import { ApiBadRequestResponse, ApiCreatedResponse, ApiNotFoundResponse, ApiResponseProperty } from '@nestjs/swagger';
+import { ApiBadRequestResponse, ApiCreatedResponse, ApiNotFoundResponse, ApiOkResponse, ApiResponseProperty } from '@nestjs/swagger';
 
 @Controller('category')
 @Role(UserRole.ADMIN)
@@ -21,22 +21,29 @@ export class CategoryController {
     return this.categoryService.create(userId, createCategoryDto);
   }
 
+  @ApiOkResponse({ description: "fetch categoriyes successfully" })
   @Get()
   findAll() {
     return this.categoryService.findAll();
   }
 
+  @ApiOkResponse({ description: "fetch category successfully" })
+  @ApiNotFoundResponse({ description: "category not found" })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.categoryService.findOne(+id);
   }
 
+  @ApiOkResponse({ description: "category updated successfully", type: UpdateCategoryResponse })
+  @ApiNotFoundResponse({ description: "category not found" })
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateCategoryDto: UpdateCategoryDto): Promise<UpdateCategoryResponse> {
     return this.categoryService.update(+id, updateCategoryDto);
   }
 
   @Delete(':id')
+  @ApiOkResponse({ description: "category removed successfully" })
+  @ApiNotFoundResponse({ description: "category not found" })
   remove(@Param('id') id: string) {
     return this.categoryService.remove(+id);
   }
