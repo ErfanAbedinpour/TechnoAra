5cf8c76238e22b968717488d21a24be26e594739
erfan <erfan.wtf44@gmail.com>
2025-02-06 14:11:16 -0500

    chore: Removing unecessery Imports

--- src/import/external.import.ts
+++ src/import/external.import.ts
@@ -1,13 +1,8 @@
 import { MikroOrmModule } from "@mikro-orm/nestjs";
-import { Logger, ValueProvider } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import databaseConfig from "src/config/database.config";
 import config from '../mikro-orm.config'
 
-const QueryLogger: ValueProvider = {
-    provide: "QueryLogger",
-    useValue: new Logger("mikro-orm")
-}
 
 
 export const externalImports = [
