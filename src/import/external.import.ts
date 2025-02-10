de08f50c39b3582946646ff770bc0dd3f3666e2c
erfan <erfan.wtf44@gmail.com>
2025-02-10 09:18:39 -0500

    chore: configuration bull module

--- src/import/external.import.ts
+++ src/import/external.import.ts
@@ -1,11 +1,24 @@
 import { MikroOrmModule } from "@mikro-orm/nestjs";
-import { ConfigModule } from "@nestjs/config";
+import { ConfigModule, ConfigType } from "@nestjs/config";
 import databaseConfig from "src/config/database.config";
 import config from '../mikro-orm.config'
+import { BullModule } from "@nestjs/bullmq";
+import redisConfig from "../config/redis.config";
 
 
 
 export const externalImports = [
-    ConfigModule.forRoot({ cache: true, isGlobal: true, load: [databaseConfig] }),
+    ConfigModule.forRoot({ cache: true, isGlobal: true, load: [databaseConfig, redisConfig] }),
     MikroOrmModule.forRoot(config),
+    BullModule.forRootAsync({
+        inject: [redisConfig.KEY],
+        useFactory: (config: ConfigType<typeof redisConfig>) => {
+            return {
+                connection: {
+                    host: config.host,
+                    port: +config.port
+                }
+            }
+        }
+    })
 ]
