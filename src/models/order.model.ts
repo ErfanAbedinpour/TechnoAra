b4d33218370aeecb2a6ff2a69e2eee8f08a1ea10
erfan <erfan.wtf44@gmail.com>
2025-03-24 17:39:47 -0400

    fix: Updating OrderModel Attributes

--- src/models/order.model.ts
+++ src/models/order.model.ts
@@ -1,27 +1,37 @@
-import { Collection, Entity, Enum, ManyToMany, ManyToOne, Property, Rel } from "@mikro-orm/core";
+import { Collection, Entity, Enum, ManyToMany, ManyToOne, OneToOne, Property, Rel } from "@mikro-orm/core";
 import { User } from "./user.model";
 import { Product } from "./product.model";
 import { OrderItem } from "./order-item.model";
 import { BaseEntity } from "./base.entity";
-
-
-@Entity({ tableName: "orders" })
-export class Order extends BaseEntity {
-    @Enum({ items: () => OrderStatus })
-    status = OrderStatus.Processing
-
-    @ManyToOne(() => User, { deleteRule: "set null" })
-    user: Rel<User>
-
-    @ManyToMany(() => Product, product => product.orders, { pivotEntity: () => OrderItem, owner: true, nullable: false })
-    products = new Collection<Product>(this)
-
-}
-
-
+import { Address } from "./address.model";
+import Decimal from "decimal.js";
 
 export enum OrderStatus {
     Processing = 'processing',
     Complete = "complete",
     Cancelled = "cancelled",
+}
+
+@Entity({ tableName: "orders" })
+export class Order extends BaseEntity {
+    @Enum({ items: () => OrderStatus, default: OrderStatus.Processing, nullable: false })
+    status = OrderStatus.Processing
+
+    @ManyToOne(() => User, { deleteRule: "set null" })
+    user: Rel<User>
+
+    @Property({ type: "uuid", nullable: false })
+    order_number: string
+
+    @Property({ type: "date", nullable: false })
+    delivered_at: Date
+
+    @OneToOne(() => Address, { nullable: false })
+    address: Address
+
+    @ManyToMany(() => Product, product => product.orders, { pivotEntity: () => OrderItem, owner: true, nullable: false })
+    products = new Collection<Product>(this)
+
+    @Property({ type: 'decimal', nullable: false })
+    total_price: Decimal
 }
